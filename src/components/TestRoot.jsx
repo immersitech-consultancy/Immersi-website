bbb8661621cd908f69b6c43f9fc72559fc5b97b5
Usp9oael <ibrafaraj55@gmail.com>
2024-05-27 07:40:55 +0100

    add(spline function): added spline function that calls from spline cdn

---
+++ src/components/TestRoot.jsx
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function TestRoot() {
+  return (
+    <Spline scene="https://prod.spline.design/ByjchLkzXHOzkwPO/scene.splinecode" />
+  );
+}
